375d36f11c26bca9ee9bd8c14d352ec51ad1e36c
Benoit de Chezelles <lesell_b@epitech.eu>
2016-11-11 21:31:54 +0100

    test: add directive tests for kooc call

--- src/directive/test_call.c
+++ src/directive/test_call.c
@@ -1,3 +1,5 @@
 int a = [MyModule function];
 int b = [MyModule function :1 :2 :3.3];
 
+int c = [MyClass method :&obj :1 :2];
+
--- test/DirectiveTest.py
+++ test/DirectiveTest.py
@@ -198,4 +198,36 @@ class DirectiveLookup(unittest.TestCase):
         self.assertEqual(lookup.context, 'MyModule')
         self.assertEqual(lookup.member, 'some_variable')
 
+class DirectiveCall(unittest.TestCase):
+
+    koocparser = directive.Directive()
+
+    def test_static_call(self):
+        """TODO: doc"""
+
+        source = """
+            int a = [MyModule some_function];
+            int b = [MyClass some_method :&obj :1 :2 :3.3];
+        """
+
+        ast = self.koocparser.parse(source)
+        self.assertIsInstance(ast.body[0], nodes.Decl)
+
+        decl1 = ast.body[0]
+        self.assertIsInstance(decl1._assign_expr, knodes.KcCall)
+
+        call = decl1._assign_expr
+        self.assertEqual(call.context, 'MyModule')
+        self.assertEqual(call.function, 'some_function')
+        self.assertListEqual(call.params, [])
+
+        decl2 = ast.body[1]
+        self.assertIsInstance(decl2._assign_expr, knodes.KcCall)
+
+        call = decl2._assign_expr
+        self.assertEqual(call.context, 'MyClass')
+        self.assertEqual(call.function, 'some_method')
+        self.assertIsInstance(call.params, list)
+        self.assertEqual(len(call.params), 4)
+
 unittest.main()
