1fe536de6a1fbb088f6e4b0a52142c9f9d0ba65b
Christopher Paccard <christopher.paccard@epitech.eu>
2016-11-21 22:27:09 +0100

    Refactor a lot of code for typing visitors

--- Kooc/passes/visitors/typing.py
+++ Kooc/passes/visitors/typing.py
@@ -15,92 +15,121 @@ class Typing(VisitorRunner):
         """Register all the visitors for the runner"""
         self.register_visitor(self.type_c_cast)
         self.register_visitor(self.resolve_expr_context)
-        self.register_visitor(self.resolve_invariance)
+        self.register_visitor(self.resolve)
         self.register_visitor(self.check_and_apply)
 
         
-    def resolve_invariance(self):
+    def resolve(self):
         """Resolve all the invariance possibilities"""
         for expr in self.ast.yield_expr():
-            self.choose_resolve_method_invariance(expr)
+            self.choose_resolve_method(expr)
 
             
-    def resolve_invariance_KcCall(self, expr):
-        """Resolve the invariance of a node of type KcCall"""
-        expr.params = self.resolve_params(expr.params)
+    def resolve_KcCall(self, kccall_expr):
+        """Resolve the typing for a KcCall node"""
+        # Check if the typing is already done
+        if hasattr(kccall_expr, "expr_type") is True and isinstance(kccall_expr.expr_type, list):
+            return kccall_expr
         
-        context = expr.context()
-        for decl in context.body:
+        kccall_expr.params = self.resolve_params(kccall_expr.params)
+        kccall_expr_type = list()
+        kccall_name = kccall_expr.function
+
+        # Search among the context the KcCall declarations by it's name
+        for decl in kccall_expr.context().body:
             if isinstance(decl, nodes.Decl) is not True or isinstance(decl._ctype, nodes.FuncType) is not True:
                 continue
-            elif decl._name == expr.function and len(decl._ctype.params) == len(expr.params):
-                is_equal = True
-                for key, param in enumerate(decl._ctype.params):
-                    if self.compare_param_type(param, expr.params[key]) is not True:
-                        is_equal = False
-                        break
-                if is_equal is True:
-                    expr.expr_type = [nodes.PrimaryType(decl._ctype._identifier)]
+            elif decl._name == kccall_name and len(decl._ctype.params) == len(kccall_expr.params) and self.compare_params_type(kccall_expr.params, decl._ctype.params) is True:
+                kccall_expr_type.append(nodes.PrimaryType(decl._ctype._identifier))
+
+        # Check if at least a type has been found and set them
+        if len(kccall_expr_type) <= 0:
+            raise KVisitorError("Cant find KcLookup '"+ kccall_name +"' for typing")
+        else:
+            kccall_expr.expr_type = kccall_expr_type
+            
+        return kccall_expr
 
                     
-    def resolve_invariance_KcLookup(self, expr):
-        """Resolve the invariance of a node of type KcLookup"""
-        if (expr.expr_type is not None):
-            return
-        context = expr.context()
-        for decl in context.body:
-            if isinstance(decl, nodes.Decl) is not True or isinstance(decl._ctype, nodes.FuncType) is True:
-                continue
-            elif decl._name == expr.member:
-                expr.expr_type = [nodes.PrimaryType(decl._ctype._identifier)]
-        return expr
+    def resolve_KcLookup(self, kclookup_expr):
+        """Resolve the typing for KcLookup node"""
+        # Check if the typing is already done
+        if hasattr(kclookup_expr, "expr_type") is True and isinstance(kclookup_expr.expr_type, list):
+            return kclookup_expr
 
-    
-    def resolve_invariance_Literal(self, expr):
-        """Resolve the invariance of a node of type Literal"""
-        return self.typed_literal.parse(expr.value)
-
-    
-    def resolve_invariance_Id(self, expr):
-        """Resolve the invariance of a node of type Id"""
-        root = self.get_root(expr)
-        for decl in root.body:
+        kclookup_expr_type = list()
+        kclookup_name = kclookup_expr.member
+        
+        # Search among the context the KcLookup declarations by it's name
+        for decl in kclookup_expr.context().body:
             if isinstance(decl, nodes.Decl) is not True or hasattr(decl, "_name") is not True:
                 continue
-            elif decl._name == expr.value:
-                expr.expr_type = [nodes.PrimaryType(decl._ctype._identifier)]
-                break;
-        # TODO : Raise error if id is not found
-        return expr
+            elif decl._name == kclookup_name:
+                kclookup_expr_type.append(nodes.PrimaryType(decl._ctype._identifier))
 
-
-    def check_and_apply(self):
-        for expr in self.ast.yield_expr():
-            self.check_and_apply_loop(expr)
+        # Check if at least a type has been found and set them
+        if len(kclookup_expr_type) <= 0:
+            raise KVisitorError("Cant find KcLookup '"+ kclookup_name +"' for typing")
+        else:
+            kclookup_expr.expr_type = kclookup_expr_type
             
-    def check_and_apply_loop(self, expr):
-        if hasattr(expr, "expr_type") is True and isinstance(expr.expr_type, list) is True:
-            if len(expr.expr_type) > 1:
-                print("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO") # Todo handle error if multiple type
-            else:
-                expr.expr_type = expr.expr_type[0]
-                if hasattr(expr, "params"):
-                    for key, param in enumerate(expr.params):
-                        expr.params[key] = self.check_and_apply_loop(param)
-        return expr
+        return kclookup_expr
 
+    
+    def resolve_Literal(self, literal_expr):
+        """Resolve the typing for Literal node"""
+        return self.typed_literal.parse(literal_expr.value)
+
+    
+    def resolve_Id(self, id_expr):
+        """Resolve the typing for an Id node"""
+        # Check if the typing is already done
+        if hasattr(id_expr, "expr_type") is True and isinstance(id_expr.expr_type, nodes.PrimaryType):
+            return id_expr
+
+        id_expr_type = None
+        id_name = id_expr.value
+
+        # Search among all declaration the id declaration by it's name
+        for decl in self.get_root(id_expr).body:
+            if isinstance(decl, nodes.Decl) is not True or hasattr(decl, "_name") is not True:
+                continue
+            elif decl._name == id_name:
+                if id_expr_type is None:
+                    id_expr_type = nodes.PrimaryType(decl._ctype._identifier)
+                else:
+                    raise KVisitorError("Ambiguous Id '"+ id_name +"' for typing")
+                    
+        # Check if a type has been found and set it
+        if id_expr_type is None:
+            raise KVisitorError("Cant find Id '"+ id_name +"' for typing")
+        else:
+            id_expr.expr_type = id_expr_type
+            
+        return id_expr
+
+
+    def resolve_Binary(self, binary_expr):
+        """Resolve the typing for a Binary node"""
+        binary_expr.params = self.resolve_params(binary_expr.params)
+        if binary_expr.params[0].expr_type._identifier != binary_expr.params[1].expr_type._identifier:
+            KVisitorError("not same type")
+            
+    
     # ~~~~~~~~~~ Utils ~~~~~~~~~~
 
-    def choose_resolve_method_invariance(self, expr):
+    def choose_resolve_method(self, expr):
         """Choose the way to resolve the invariance by type of node"""
         if isinstance(expr, knodes.KcCall):
-            return self.resolve_invariance_KcCall(expr)
+            return self.resolve_KcCall(expr)
         elif isinstance(expr, knodes.KcLookup):
-            return self.resolve_invariance_KcLookup(expr)
+            return self.resolve_KcLookup(expr)
         elif isinstance(expr, nodes.Literal):
-            return self.resolve_invariance_Literal(expr)
+            return self.resolve_Literal(expr)
         elif isinstance(expr, nodes.Id):
-            return self.resolve_invariance_Id(expr)
+            return self.resolve_Id(expr)
+        elif isinstance(expr, nodes.Binary):
+            return self.resolve_Binary(expr)
         else:
             print("Unknow way to resolve expression: "+ expr.__class__.__name__)
             return expr
@@ -114,15 +143,42 @@ class Typing(VisitorRunner):
         return root
 
 
-    def compare_param_type(self, param_decl, expr_decl):
-        return param_decl == expr_decl
-
+    def compare_params_type(self, kccall_params, decl_params):
+        """Compare if the params are the same types on the two given list of params"""
+        is_equal = True
+        for key, decl_param in enumerate(decl_params):
+            kccall_param_expr_type = kccall_params[key].expr_type[0] if isinstance(kccall_params[key].expr_type, list) else kccall_params[key].expr_type # TODO : Fixme :( => Invariance
+            if kccall_param_expr_type._identifier != decl_param._ctype._identifier:
+                is_equal = False
+        return is_equal
+            
     
     def resolve_params(self, params):
         """Resolve the types of the params"""
         for key, param in enumerate(params):
-            params[key] = self.choose_resolve_method_invariance(param)
+            params[key] = self.choose_resolve_method(param)
         return params
+
+
+    def check_and_apply(self):
+        for expr in self.ast.yield_expr():
+            self.check_and_apply_loop(expr)
+
+            
+    def check_and_apply_loop(self, expr):
+        if hasattr(expr, "expr_type") is True:
+            if isinstance(expr.expr_type, list) is True:
+                if len(expr.expr_type) > 1:
+                    raise KVisitorError("Multiple type spotted !!")
+                else:
+                    expr.expr_type = expr.expr_type[0]
+                    if hasattr(expr, "params"):
+                        for key, param in enumerate(expr.params):
+                            expr.params[key] = self.check_and_apply_loop(param)
+            elif expr.expr_type is None:
+                raise KVisitorError("No type spotted !!")
+        return expr
+
 
 
     # ~~~~~~~~~~ Shit of Lesell ~~~~~~~~~~
